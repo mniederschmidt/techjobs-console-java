9f1783c10af45e462b9558b2a43e94c554b36bee
Mary Niederschmidt <mary.niederschmidt@gmail.com>
2017-03-09 13:23:21 -0600

    Implemented printJobs functionality

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -111,6 +112,11 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        for (HashMap<String, String> job : someJobs) {
+            for (Map.Entry<String, String> field : job.entrySet()) {
+                System.out.println(field.getKey() + ": " + field.getValue());
+            }
+            System.out.println("*****");
+        }
     }
 }
